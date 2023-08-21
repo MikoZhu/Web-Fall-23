e5470a66fa072db4b0ca664d0ed0eb2e948a24c0
Diego Zito <diegozito.m@gmail.com>
2023-08-21 17:53:04 +0200

    w1d1 content added

---
+++ weeks/week-1/explanations/0-basics.js
@@ -0,0 +1,14 @@
+// JS Basics
+
+// --------------
+// --------------
+// --------------
+// What is Javascriot ?
+
+// JavaScript is a programming language that allows web developers to add interactivity and functionality to their websites. It's executed directly in a web browser and enables you to control how elements on a webpage behave and respond to user actions. JavaScript is a crucial tool for creating dynamic and engaging web experiences.
+
+// Why JavaScript?
+// JavaScript is essential because it brings websites to life. Without it, web pages would be static and lack interaction. This is done through a concept called a JIT Compiler, which means "Just in Time"
+
+// What is a compiler
+// Imagine a compiler as a special tool that turns fancy code made by us programmers into simple instructions that computers can follow. This happens through something called Just-In-Time Compilation, where a browser's engine changes the human-friendly code into a super-efficient version that makes things work better on the computer.
---
+++ weeks/week-1/explanations/1-dataTypes.js
@@ -0,0 +1,26 @@
+// Data Types
+
+// There are different types of data that we can use in a JavaScript.
+
+// To be clear there are 2 types of data cagegories:
+// - Primitive Data Types
+// - Non-Primitive Data Types
+
+// In simple terms, primitive data types represent only one value while non primitive data types are the collections of primitive data types
+
+// Primitive Data Types:
+// Strings - Represents textual data
+// Number - An integer or a floating-point number
+// BigInt - An integer with arbitrary precision - is a special type of number that allows you to work with really, really big whole numbers.
+// Boolean - Any of two values: true or false
+// Undefined - A data type whose variable is not defined or initialized
+// Null -  A null value
+
+// Non Primitive Data Types:
+// Arrays - Container of multiples values of collection of data
+// Objects - Container with key-value pairs of collection of data
+
+// IMPORTANT!!!!
+// Showcase the use of the reserved keyword typeof
+// The typeof operator returns a string indicating the type of the operand's value.
+// Is like a detective that helps you figure out what type of thing something is. It tells you if something is a number, a word, a function, or something else. It's like looking at an object and saying, "Hey, what are you?" to understand what kind of thing you're dealing with in your code.
