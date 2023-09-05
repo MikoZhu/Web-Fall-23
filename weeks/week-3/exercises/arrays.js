0e3520a493c24eb0de0073b4ec3b81087efe415a
Jennie Dalgren <jennie.dalgren@gmail.com>
2023-09-05 15:18:39 +0200

    week 3 starts

---
+++ weeks/week-3/exercises/arrays.js
@@ -0,0 +1,110 @@
+const pokemons = [
+  "Bulbasaur",
+  "Ivysaur",
+  "Venusaur",
+  "Charmander",
+  "Charmeleon",
+  "Charizard",
+  "Squirtle",
+  "Wartortle",
+  "Blastoise",
+  "Metapod",
+  "Weedle",
+  "Pikachu",
+  "Pidgey",
+];
+
+// console.log("Iteration 01");
+// console.log("----");
+
+
+// console.log("Iteration 02");
+// console.log("----");
+
+
+// console.log("Iteration 03");
+// console.log("----");
+
+// console.log("Iteration 04");
+// console.log("----");
+
+
+// console.log("Iteration 05");
+// console.log("----");
+
+
+// console.log("Iteration 06");
+// console.log("----");
+
+
+// console.log("Iteration 07");
+// console.log("----");
+
+
+// console.log("Iteration 08");
+// console.log("----");
+
+
+// console.log("Iteration 09");
+// console.log("----");
+
+
+// console.log("Iteration 10");
+// console.log("----");
+
+
+// console.log("Iteration 11 ")
+// console.log("----");
+
+
+console.log("Bonus");
+// ***BONUS***
+
+// KIWI
+//const theLongName = () => {
+//   // find the pokemon with the longest name
+//   return pokemons.reduce(function (a, b) {
+//     return a.length > b.length ? a : b;
+//   });
+// };
+
+// console.log(theLongName());
+
+// ORANGES
+// const theLongestName = () => {
+//   // find the pokemon with the longest name
+//   let longestName = ""
+//   pokemons.forEach((pokemon) => {
+//     if (pokemon.length > longestName.length) {
+//       longestName = pokemon
+//     }
+//   })
+//   console.log(`The Pokemon ${longestName} has the longest name`)
+// };
+// theLongestName()
+
+// LEMONS
+// const theLongestName = () => {
+//   // find the pokemon with the longest name
+//   let longestName = []
+//   let longestSort = pokemons.sort((a, b) => b.length - a.length);
+//   for (let derp of longestSort) {
+//     if (derp.length === longestSort[0].length) {
+//       longestName.push(derp)
+//     }
+//   }
+//   console.log(longestName)
+// };
+// theLongestName()
+
+
+
+// FOR LOOP:
+// for (let i = 0; i < 5; i++) {
+//   console.log('looping', i)
+// }
+
+
+
+
+console.log("----");
---
+++ weeks/week-3/exercises/objects.js
@@ -0,0 +1,92 @@
+const pokemons = [
+  "Bulbasaur",
+  "Ivysaur",
+  "Venusaur",
+  "Charmander",
+  "Charmeleon",
+  "Charizard",
+  "Squirtle",
+  "Wartortle",
+  "Blastoise",
+  "Metapod",
+  "Weedle",
+  "Pikachu",
+  "Pidgey",
+];
+
+// 1)
+// create a loop that prints all
+// the pokemons from the pokemons array
+// invoke the function to see that it works
+
+console.log("Iteration 01");
+
+
+console.log("----");
+
+
+
+// 2)
+// Create an object for the pokemon Pikachu.
+// it could have properties such as name, element,
+// strength, color, amountOfLegs or something like that.
+console.log("Iteration 02");
+
+
+console.log("----");
+
+
+// 3)
+// console.log something like
+// 'Pikachu is a electricity pokemon with the strength of 32'
+// Selecting some of the information about your pikachu object.
+console.log("Iteration 03");
+
+
+console.log("----");
+
+// 4)
+// Add the property stillToCatch: true to the Pikachu object.
+console.log("Iteration 04");
+
+
+console.log("----");
+
+// 5)
+// change the value of the key strength in the Pikachu object.
+console.log("Iteration 05");
+
+
+console.log("----");
+
+// 6)
+// Delete a property from the Pikachu object.
+console.log("Iteration 06");
+
+
+console.log("----");
+
+// 7)
+// Fill up this array with a few more pokemons.
+// It is an array with objects!
+// I made some suggestions
+// but feel free to change as you want.
+console.log("Iteration 07");
+
+console.log("----");
+
+
+// 8)
+// Print out all the NAMES of the pokemons in this new array.
+console.log("Iteration 08");
+
+
+console.log("----");
+
+
+// 9)
+// Print out something like
+// 'Bulbasaur is a water pokemon with the strength of 32'
+// Do this for all the pokemons.
+console.log("Iteration 09");
+
