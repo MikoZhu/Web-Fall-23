85def52abf7294dd56119967b3b08095a31ebb80
Diego Zito <diegozito.m@gmail.com>
2023-09-26 16:39:05 +0200

    w6 - tuesday class added

--- weeks/week-6/useStateClass/src/components/Data.jsx
+++ weeks/week-6/useStateClass/src/components/Data.jsx
@@ -1,34 +1,97 @@
 import { useState } from "react";
 
 export const Data = () => {
-  // Number Data Type
-  const [count, setCount] = useState(0); // rep of a number type :)
-
-  const increment = () => setCount((count) => count + 1);
-
-  // using a text data type
+  // Primitive Data Types
+  const [count, setCount] = useState(0);
   const [text, setText] = useState("");
-  const updateText = () => setText(text + " I love coding  ");
-
-  // Using a boolean data type
   const [isVisible, setIsVisible] = useState(false);
-  const toggleBoolean = () => setIsVisible(!isVisible);
+
+  // Arrays
+  const [items, setItems] = useState([]);
+
+  // Objects
+  const [user, setUser] = useState({ name: "", age: 0 });
+
+  // Complex Data Structures
+  const [users, setUsers] = useState([
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+  ]);
+
+  // Functions
+  const [callback, setCallback] = useState(() =>
+    console.log("Default function")
+  );
   return (
-    <div className="card">
-      {/* Number Type */}
-      <button onClick={increment}>count is {count}</button>
-      {/* String/text Type */}
+    <div>
+      {/* Number */}
       <div>
-        <p>Text is: {text}</p>
-        <button onClick={updateText}>Add Text </button>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
       </div>
+
+      {/* String */}
+      <div>
+        <p>Text: {text}</p>
+        <button onClick={() => setText(text + " Hello")}>Append "Hello"</button>
+      </div>
+
       {/* Boolean */}
       <div>
-        <p>
-          Visbility is currently:{" "}
-          {isVisible ? "is visibile" : "is currently not visible"}
-        </p>
-        <button onClick={toggleBoolean}>Turn Visibile</button>
+        <p>Visibility: {isVisible ? "Visible" : "Not Visible"}</p>
+        <button onClick={() => setIsVisible(!isVisible)}>
+          Toggle Visibility
+        </button>
+      </div>
+
+      {/* Arrays */}
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <button onClick={() => setItems([...items, "New Item"])}>
+          Add Item
+        </button>
+      </div>
+
+      {/* Objects */}
+      <div>
+        <p>Name: {user.name}</p>
+        <p>Age: {user.age}</p>
+        <button onClick={() => setUser({ ...user, name: "John" })}>
+          Set Name to "John"
+        </button>
+        <button onClick={() => setUser({ ...user, age: 30 })}>
+          Set Age to 30
+        </button>
+      </div>
+
+      {/* Complex Data Structures */}
+      <div>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>
+              {user.name} - {user.age}
+            </li>
+          ))}
+        </ul>
+        <button
+          onClick={() => setUsers([...users, { name: "Charlie", age: 35 }])}
+        >
+          Add User "Charlie"
+        </button>
+      </div>
+
+      {/* Functions */}
+      <div>
+        <button onClick={callback}>Run Callback</button>
+        <button
+          onClick={() => setCallback(() => console.log("Updated function"))}
+        >
+          Update Callback
+        </button>
       </div>
     </div>
   );
