78b7c1351f64a1ec77ea967252089cd95c006bcd
Diego Zito <diegozito.m@gmail.com>
2023-10-03 13:24:23 +0200

    updated - w7 ready for class on tuesday

---
+++ weeks/week-7/class-examples/week-seven-class/src/App.jsx
@@ -0,0 +1,10 @@
+export const App = () => {
+  return (
+    <>
+      <h1>Learning the useEffect() Hook in react</h1>
+      <div className="card">
+        <p>place comps here...</p>
+      </div>
+    </>
+  );
+};
