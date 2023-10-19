424f5e12715df491c4163f5b722100450d5ac677
Diego Zito <diegozito.m@gmail.com>
2023-10-19 16:00:33 +0200

    w9 - monorepo folder added

---
+++ weeks/week-9/Q&A/monorepo-example/frontend/README.md
@@ -0,0 +1,11 @@
+### Explanation
+
+Create a .env at the root of this folder and add your env Variables :)
+
+Like this:
+
+VITE_OPENDB_KEY=yourKeyHere
+
+### example
+
+VITE_OPENDB_KEY=b8b70837ecdf33d41538d9964f7c8a94
--- weeks/week-9/Q&A/week-8-qa-session/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { QuestionDistributive } from "./components/questions/QuestionDistributive";
-export const App = () => {
-  return (
-    <>
-      <h3 className="app-heading">Question Set</h3>
-      <QuestionDistributive />
-    </>
-  );
-};
