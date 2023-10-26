ca0d216466552b38444f93a9da7678a30bbf40aa
Diego Zito <diegozito.m@gmail.com>
2023-10-26 13:53:41 +0200

    w10 - q&a added

--- weeks/week-10/Q&A/router-wTen/src/pages/Home.jsx
+++ weeks/week-10/Q&A/router-wTen/src/pages/Home.jsx
@@ -4,37 +4,15 @@ import Logos from "../components/Logos";
 
 export const Home = () => {
   const text = {
-    heading: "Vite + React + React Router + Minimal CSS",
+    heading: "useNavigate vs Link in react router",
     subheading: "Home Page",
-    intro:
-      "Welcome, Technigo students! This boilerplate is designed specifically for you. We understand the importance of a smooth start, especially when diving into libraries like React Router. With this in mind, we've crafted an out-of-the-box solution to help you seamlessly integrate the router library into your projects.",
-    cleanup: "If you want to remove all stylings you can do the following:",
+    intro: "",
+    cleanup: "",
     html: ` <ul>
     <li>
-      Comment line 5 of this file which pertains to the import of the CSS
-      file called <strong>App.css</strong> with the
-      <code>import "./App.css";</code>
+      Start navigating around...
     </li>
-    <li>
-      Navigate to the file with the name <strong>main.jsx</strong> and
-      comment/remove line 4 which pertains to the import of the CSS file
-      called <strong>index.css</strong> with the
-      <code>import "./index.css";</code>
-    </li>
-    <li>
-      Remove all logos from this file like the Technigo, React or Vite Logo
-      and proceed to remove their imports as well as remove the files
-    </li>
-    <li>
-      After these steps have being performed, we recommend removing these
-      files to keep the folder structure minimal and easier to navigate.
-    </li>
-    <li>
-      If you are feeling lazy you use this
-      <a href="https://github.com/Technigo/react-router-boiler-plate-no-css" target="_blank" rel="noopener noreferrer">
-        minimal starter with no styling
-      </a>
-    </li>
+    
   </ul>`,
   };
 
